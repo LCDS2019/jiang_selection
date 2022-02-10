7076f38543efba50328b6b41fd1ef3de8cf8e669
LCDS2019 <lcds.gru@gmail.com>
2022-02-10 01:22:55 -0300

    Version 2.1 - Iniciando a linha 7

--- z25_ccsm.py
+++ z25_ccsm.py
@@ -20,18 +20,21 @@ def ccsm(data,nodes,db_system,ontologia):
     for i in data:
         for j in data:
             ans=sim_spath(data,i,j)
-            #print(i,j,ans)
-            M.append(ans)
-            C.append((i,j,ans))
+            print(i,j,ans)
+            M.append((i,j,"{:.2f}".format(float(ans))))
+            #C.append((i,j,ans))
 
-    M=np.array(M)
-    M=np.resize(M, (nodes,nodes))
+    #M=np.array(M)
+    #M=np.resize(M, (nodes,nodes))
 
-    df = pd.DataFrame(M)
-    df.to_csv(db_system+str(ontologia.split('.')[0])+'_ccsm.csv',sep='|')
+    df = pd.DataFrame(M, columns=['C1', 'C2', 'Ans'])
 
-    dfC = pd.DataFrame(C)
-    dfC.to_csv(db_system+str(ontologia.split('.')[0])+'_ccsm_concepts.csv',sep='|')
+    df = df.pivot(values='Ans', index='C1', columns='C2')
+    print(df)
+    df.to_csv(db_system + str(ontologia.split('.')[0]) + '_ccsm.csv', sep='|')
+
+    #dfC = pd.DataFrame(C)
+    #dfC.to_csv(db_system+str(ontologia.split('.')[0])+'_ccsm_concepts.csv',sep='|')
 
     #print(M)
 
